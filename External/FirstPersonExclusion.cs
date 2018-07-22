d43f6de3fc7dd66dd10433339ac17602e2e76a84
Joelixny <joeliny_9@teknik.io>
2018-07-22 11:19:25 -0600

    Gutted plugin to be usable in Unity 2018

--- External/FirstPersonExclusion.cs
+++ External/FirstPersonExclusion.cs
@@ -1,10 +1,4 @@
-﻿using CustomAvatar;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace AvatarScriptPack
 {
@@ -14,21 +8,7 @@ namespace AvatarScriptPack
 
         void Start()
         {
-            try
-            {
-                if (Plugin.fpsAvatar && Exclude != null)
-                {
-                    foreach (Transform child in gameObject.GetComponentsInChildren<Transform>())
-                    {
-                        if (child.gameObject.layer == 27 && Exclude.Contains(child.gameObject))
-                            child.gameObject.layer = 26;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message + "\n" + ex.StackTrace);
-            }
+
         }
     }
 }
